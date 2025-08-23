45394807776ec1e82083a25aece6879d0a1779b1
Eroniction14 <eroniction14112000@gmail.com>
2025-08-23 10:56:47 -0400

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   const path = location.pathname.split("/");
   return (
     <Container>
-      GenAI
+      Image Generator
       {path[1] === "post" ? (
         <Button
           onClick={() => navigate("/")}
